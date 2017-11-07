9ed665e54e141429cf114b1f1f880c8629ec21b8
tKimura <c501737019@tokushima-u.ac.jp>
2017-11-07 13:06:50 +0900

    編集ボタンを押すことで画面遷移するように設定

---
+++ app/src/main/java/com/b5/voicecontroll/presenter/activity/editActivity.java
@@ -0,0 +1,15 @@
+package com.b5.voicecontroll.presenter.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.b5.voicecontroll.R;
+
+public class editActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}

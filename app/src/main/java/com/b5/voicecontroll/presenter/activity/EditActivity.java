541a259dec279c9a2582ba926e734e29a036c47e
tKimura <c501737019@tokushima-u.ac.jp>
2017-11-09 19:30:56 +0900

    最終修正と時間設定時のxmlへの紐づけを行った

--- app/src/main/java/com/b5/voicecontroll/presenter/activity/EditActivity.java
+++ app/src/main/java/com/b5/voicecontroll/presenter/activity/EditActivity.java
@@ -5,13 +5,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.TimePicker;
-
+import java.util.Locale;
 import com.b5.voicecontroll.R;
 
 import java.util.Calendar;
 
 public class EditActivity extends AppCompatActivity {
+    private String startTime = "startTime";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,16 +28,21 @@ public class EditActivity extends AppCompatActivity {
     public void decideTimes(View view) {
         // 現在時刻を取得
         Calendar calendar = Calendar.getInstance();
-        final int hour = calendar.get(Calendar.HOUR_OF_DAY);
-        final int minute = calendar.get(Calendar.MINUTE);
+        int hour = calendar.get(Calendar.HOUR_OF_DAY);
+        int minute = calendar.get(Calendar.MINUTE);
 
         // 時間選択ダイアログの生成
         TimePickerDialog timepick = new TimePickerDialog(
                 this,
                 new TimePickerDialog.OnTimeSetListener() {
                     public void onTimeSet(TimePicker view,
-                                          int hourOfDay, int minute) {
-                        // 設定 ボタンクリック時の処理
+                                          int hourOfDay, int minute) {// 設定 ボタンクリック時の処理
+                        // 時と分を文字列として結合
+                        String timeStr = String.format(Locale.US, "%d:%d", hourOfDay, minute);
+
+                        // 紐づけ
+                        TextView setTime = (TextView)findViewById(R.id.time_decision); //
+                        setTime.setText(timeStr);
                     }
                 },
                 hour,
@@ -44,6 +51,10 @@ public class EditActivity extends AppCompatActivity {
 
         // 表示
         timepick.show();
+
+
+
+
     }
 }
 

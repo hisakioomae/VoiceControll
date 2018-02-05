cd5c767ac17c024395a2426a7ddc1ec1cb0441e5
木村拓也 <kimuratakuya@Mac.local>
2018-02-05 16:17:31 +0900

    繰り返し機能の追加

--- app/src/main/java/com/b5/voicecontroll/presenter/presenter/activity/MainActivity.java
+++ app/src/main/java/com/b5/voicecontroll/presenter/presenter/activity/MainActivity.java
@@ -64,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
                 ListItem item = (ListItem) parent.getItemAtPosition(position);
                 times = item.getTimeBox();
                 String day = item.getDay();
+                clearAlarm(item.getId());
                 timeEdit(position, day);
             }
         });
@@ -158,8 +159,7 @@ public class MainActivity extends AppCompatActivity {
     public void timeEdit(int position, String day) {
         Intent intent = new Intent(this, EditActivity.class);
         intent.putExtra("edit_times", times);
-        System.out.println("main: " + day);
-        intent.putExtra("select_day", day);
+        intent.putExtra("select_day", day);  // TODO: indexを送ることで省略できそう
         intent.putExtra("list_position", position);
         startActivityForResult(intent, EDIT_CODE);
     }
@@ -177,7 +177,6 @@ public class MainActivity extends AppCompatActivity {
         intent.setType(String.valueOf(item.getId()));
         PendingIntent sender = PendingIntent.getBroadcast(MainActivity.this, 0, intent, 0);
 
-
         // 現在の時刻をcalendarにセット
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
@@ -187,12 +186,22 @@ public class MainActivity extends AppCompatActivity {
         calendar_target.setTimeInMillis(System.currentTimeMillis());
         calendar_target.set(Calendar.HOUR_OF_DAY, item.getTimeBox()[0]);
         calendar_target.set(Calendar.MINUTE, item.getTimeBox()[1]);
+        calendar_target.set(Calendar.DAY_OF_WEEK, 1);
 
-        // ターゲット時刻が何秒後かを追加
-        calendar.add(Calendar.SECOND, returnSecond(calendar, calendar_target));
+        // ターゲットと現在の曜日差を取得
+        dayDiff(calendar.get(Calendar.DAY_OF_WEEK), convertDay(item.getDay()));
 
         AlarmManager am = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        am.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), sender);
+        // ターゲット時刻が何秒後かを追加
+        calendar.add(Calendar.SECOND, returnSecond(calendar, calendar_target, dayDiff(calendar.get(Calendar.DAY_OF_WEEK), convertDay(item.getDay()))));
+
+        // 繰り返しあり・なしで場合分け
+        if (item.getDay().equals("しない")) {
+            // ターゲット時刻が何秒後かを追加
+            am.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), sender);
+        } else {
+            am.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 86400000 * 7, sender);
+        }
     }
 
     /**
@@ -214,9 +223,10 @@ public class MainActivity extends AppCompatActivity {
      *
      * @param calendar        現在の時間情報を含んだCalendar型変数
      * @param calendar_target 設定時間の情報を含んだCalendar型変数
+     * @param dayDiff         指定時間と現在の曜日の差
      * @return 設定時刻までの秒数
      */
-    public int returnSecond(Calendar calendar, Calendar calendar_target) {
+    public int returnSecond(Calendar calendar, Calendar calendar_target, int dayDiff) {
         int hourDiff = Math.abs(calendar_target.get(Calendar.HOUR_OF_DAY) - calendar.get(Calendar.HOUR_OF_DAY));
         int minuteDiff = Math.abs(calendar_target.get(Calendar.MINUTE) - calendar.get(Calendar.MINUTE));
 
@@ -245,8 +255,40 @@ public class MainActivity extends AppCompatActivity {
                 hourDiff = 0;
             }
         }
+        return ((dayDiff - 1) * 86400) + (hourDiff * 3600) + (minuteDiff * 60) - calendar.get(Calendar.SECOND) - 2;
+    }
 
-        return (hourDiff * 3600) + (minuteDiff * 60) - calendar.get(Calendar.SECOND) - 2;
+    /**
+     * ListViewに格納されている曜日を文字列からCalendarクラス変数に置換
+     *
+     * @param day ListViewに格納されている曜日文字列
+     * @return 置換後の曜日
+     */
+    public int convertDay(String day) {
+        int index = 0;
+        if (day.equals("日曜日")) {
+            index = 1;
+        } else if (day.equals("月曜日")) {
+            index = 2;
+        } else if (day.equals("火曜日")) {
+            index = 3;
+        } else if (day.equals("水曜日")) {
+            index = 4;
+        } else if (day.equals("木曜日")) {
+            index = 5;
+        } else if (day.equals("金曜日")) {
+            index = 6;
+        } else if (day.equals("土曜日")) {
+            index = 7;
+        }
+        return index;
+    }
+
+    public int dayDiff(int current, int target) {
+        int diff = target - current;
+        if (diff < 0) diff += 7;
+        if (target == 0) diff = 0;
+        return diff;
     }
 
     /**

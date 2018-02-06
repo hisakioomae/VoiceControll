e08cdb8cfc816bea38be6e433e0d681a9aac51d6
木村拓也 <kimuratakuya@Mac.local>
2018-02-06 21:02:09 +0900

    SwitchのID変更

--- app/src/main/java/com/b5/voicecontroll/presenter/presenter/activity/MainActivity.java
+++ app/src/main/java/com/b5/voicecontroll/presenter/presenter/activity/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
+import android.preference.SwitchPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
